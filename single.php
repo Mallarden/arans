299e51ca78b710e525cba6c4928f26928ae7e1ab
Mallarden <antonanderson.com@gmail.com>
2019-11-21 14:50:09 +0100

    removed author

--- single.php
+++ single.php
@@ -12,10 +12,10 @@ get_header();
 <div class="single">
 		<div class="container">
 				<h1 style="font-family: 'Abril Fatface', cursive;" class="pb-2"><?php echo the_title(); ?> </h1>
-				<div class="blog-author pb-3">
+				<!-- <div class="blog-author pb-3">
 					<span class="dashicons-calendar dashicons"></span><div class="date p-1"><?php the_date(); ?></div>
 					<span class="ml-2 dashicons-admin-users dashicons"></span><div class="author p-1"><?php the_author(); ?></div>
-				</div>
+				</div> -->
 			<div class="">
 				<?php echo the_content(); ?>
 			</div>
